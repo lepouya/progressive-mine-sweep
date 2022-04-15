01a75622580687f0c5f778704cd9af23cf40b3d0
lepouya <pouya.larjani@gmail.com>
2022-04-15 01:05:18 -0700

    Smarter icon handling

--- src/utils/Icon.tsx
+++ src/utils/Icon.tsx
@@ -1,52 +1,67 @@
 import React from "react";
-import * as TI from "@tabler/icons";
+import * as TablerIcons from "@tabler/icons";
 import { Cell, CellState } from "../model/Cell";
 
-export const Icon: React.FC<TI.TablerIconProps & { icon: string }> = (props) =>
-  icons[props.icon](props);
+export const Icon: React.FC<TablerIcons.TablerIconProps & { icon: string }> = (
+  props,
+) => {
+  const parts = props.icon.split(/\s+/).filter((s) => s.trim().length > 0);
+  const partProps: Record<string, any> = {};
+  const getPart = (name: string, finder: (value: string) => boolean) => {
+    const found = parts.findIndex(finder);
+    return found >= 0 ? (partProps[name] = parts.splice(found, 1)[0]) : null;
+  };
 
-export const CellIcon: React.FC<
-  TI.TablerIconProps & { cell?: Cell; state?: CellState; neighbors?: number }
-> = (props) => {
-  const state = props.cell?.state ?? props.state ?? "hidden";
-  const neighbors = props.cell?.neighbors ?? props.neighbors ?? 0;
-  const color = cellIcons[state].color;
-  let icon = cellIcons[state].icon;
-  if (icon instanceof Array) {
-    icon = icon[neighbors];
-  }
+  const icon = getPart("icon", (s) => getIcon(s) != null);
+  getPart("size", (s) => !isNaN(parseFloat(s)));
+  getPart("color", (s) => s.startsWith("#") || isNaN(parseFloat(s)));
+  getPart("stroke", (s) => !isNaN(parseFloat(s)));
 
-  return icons[icon]({ color, ...props });
+  return icon ? getIcon(icon)({ ...partProps, ...props }) : null;
 };
 
-export const icons: Record<string, TI.TablerIcon> = {};
-for (const [name, icon] of Object.entries(TI)) {
-  icons[name] = icon;
-  if (name.startsWith("Icon")) {
-    icons[name.substring(4)] = icon;
+export const CellIcon: React.FC<
+  TablerIcons.TablerIconProps & {
+    cell?: Cell;
+    state?: CellState;
+    neighbors?: number;
   }
+> = (props) => {
+  let icon = cellIcons[props.cell?.state ?? props.state ?? "hidden"];
+  if (icon instanceof Array) {
+    icon = icon[props.cell?.neighbors ?? props.neighbors ?? 0];
+  }
+  return Icon({ ...props, icon });
+};
+
+export const getIcon = (name: string) =>
+  icons[name] ??
+  icons[name.toLowerCase()] ??
+  icons[name.toLowerCase().replace("icon", "")];
+
+const icons: Record<string, TablerIcons.TablerIcon> = {};
+for (const [name, icon] of Object.entries(TablerIcons)) {
+  icons[name] = icon;
+  icons[name.toLowerCase().replace("icon", "")] = icon;
 }
 
 const cellIcons = {
-  hidden: { color: "transparent", icon: "QuestionMark" },
-  hinted: { color: "green", icon: "Eye" },
-  flagged: { color: "darkblue", icon: "Flag2" },
-  blown: { color: "red", icon: "AlertTriangle" },
-  revealed: {
-    color: "darkgreen",
-    icon: [
-      "SquareDot",
-      "Square1",
-      "Square2",
-      "Square3",
-      "Square4",
-      "Square5",
-      "Square6",
-      "Square7",
-      "Square8",
-      "Square",
-    ],
-  },
+  hidden: "transparent QuestionMark",
+  hinted: "green Eye",
+  flagged: "darkblue Flag2",
+  blown: "red AlertTriangle",
+  revealed: [
+    "darkgreen SquareDot",
+    "darkgreen Square1",
+    "darkgreen Square2",
+    "darkgreen Square3",
+    "darkgreen Square4",
+    "darkgreen Square5",
+    "darkgreen Square6",
+    "darkgreen Square7",
+    "darkgreen Square8",
+    "darkgreen Square",
+  ],
 };
 
 export default Icon;
