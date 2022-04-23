f50cba0b891f2448633988c6d2e2eb5a111de189
lepouya <pouya.larjani@gmail.com>
2022-04-23 01:03:51 -0700

    Show difficulty and hint quality as percentages

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -5,9 +5,10 @@ import ResourceRender from "../utils/ResourceRender";
 
 const Stats: React.FC = () => {
   const { resource } = useGameContext();
-  const render = (name: string) => (
+  const render = (name: string, display?: "number" | "time" | "percentage") => (
     <ResourceRender
       resource={resource(name)}
+      display={display}
       length={"compact"}
       showMaxValue={true}
       showRate={true}
@@ -29,8 +30,8 @@ const Stats: React.FC = () => {
         <div className="title-bar">Board Info</div>
         {render("rows")}
         {render("cols")}
-        {render("difficulty")}
-        {render("hintQuality")}
+        {render("difficulty", "percentage")}
+        {render("hintQuality", "percentage")}
       </div>
       <div className="panel stats">
         <div className="title-bar">Game Info</div>
