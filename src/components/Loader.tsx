4054f1fb4d2458c402c6c5002e1f4052a746ed44
lepouya <pouya.larjani@gmail.com>
2022-05-25 01:23:10 -0700

    Option for full game reset when load fails

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,23 +3,36 @@ import React, { useEffect, useState } from "react";
 import Spinner from "./Spinner";
 import Game from "./Game";
 import useGameContext from "./GameContext";
+import Reset from "../pages/Reset";
 
 const Loader: React.FC = () => {
   const { load } = useGameContext();
   const [isLoaded, setLoaded] = useState(false);
+  const [isFailed, setFailed] = useState(false);
 
   useEffect(() => {
-    if (!isLoaded) {
+    if (!isLoaded && !isFailed) {
       setTimeout(initialize, 1);
     }
-  }, []);
+  }, [isLoaded, isFailed]);
 
   function initialize() {
-    load();
-    setLoaded(true);
+    try {
+      load();
+      setLoaded(true);
+    } catch (ex) {
+      setFailed(true);
+      throw ex;
+    }
   }
 
-  return isLoaded ? <Game /> : <Spinner />;
+  if (isLoaded) {
+    return <Game />;
+  } else if (isFailed) {
+    return <Reset />;
+  } else {
+    return <Spinner />;
+  }
 };
 
 export default Loader;
---
+++ src/pages/Reset.tsx
@@ -0,0 +1,50 @@
+import { useMemo } from "react";
+import useGameContext from "../components/GameContext";
+
+export default function Reset() {
+  const context = useGameContext();
+  const saveData = useMemo(() => context.saveAs(true), []);
+
+  function resetGame() {
+    if (confirm("Are you sure you want to reset? This cannot be undone.")) {
+      context.reset();
+      window.location.reload();
+    }
+  }
+
+  return (
+    <div>
+      <div className="panel options">
+        <div className="title-bar">Load Failure!</div>
+        <div className="full">
+          Looks like something went very wrong in loading the game. It's
+          possible that the game version is out of date, or that the save data
+          is corrupt.
+        </div>
+        <hr className="separator" />
+        <div className="full">
+          For your records, here is the current contents of the save state. Keep
+          this somewhere before resetting the game:
+        </div>
+        <div className="full center">
+          <textarea value={saveData} readOnly={true} />
+        </div>
+      </div>
+
+      <div className="panel options reset">
+        <div className="title-bar warning">Game Reset</div>
+        <div className="full">
+          WARNING: this will completely reset your game and delete all saved
+          progress and settings. This will hard reset the game from beginning.
+        </div>
+        <div className="full right">
+          <input
+            type="button"
+            value="Hard Reset Everything"
+            onClick={resetGame}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
