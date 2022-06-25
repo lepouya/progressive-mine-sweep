94c732ea908420e53674d629e50a19c7daef9e1c
Pouya Larjani <plj@fb.com>
2022-06-24 20:28:07 -0700

    ProgressBar component

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,47 @@
+import { PropsWithChildren } from "react";
+
+export type ProgressBarProps = PropsWithChildren<{
+  value: number;
+  minValue?: number;
+  maxValue?: number;
+  orientation?: "horizontal" | "vertical";
+  width?: string | number;
+  height?: string | number;
+  showPercent?: boolean;
+  className?: string;
+}>;
+
+export default function ProgressBar({
+  value,
+  minValue = 0.0,
+  maxValue = 1.0,
+  orientation = "horizontal",
+  width = orientation === "horizontal" ? "100%" : "1ex",
+  height = orientation === "vertical" ? "100%" : "1ex",
+  showPercent = false,
+  className,
+  children,
+}: ProgressBarProps) {
+  const pct = (value - minValue) / (maxValue - minValue);
+  const endX = orientation === "horizontal" ? pct : 1;
+  const endY = orientation === "vertical" ? pct : 1;
+
+  return (
+    <div
+      className={`progress-bar ${className ?? ""}`}
+      style={{ minWidth: width, minHeight: height }}
+    >
+      <div className="children">
+        {showPercent && <span>{Math.floor(100 * pct)}%</span>}
+        {children}
+      </div>
+      {pct > 0.001 && (
+        <div
+          className="primary"
+          style={{ width: `${endX * 100}%`, height: `${endY * 100}%` }}
+        ></div>
+      )}
+      <div className="secondary"></div>
+    </div>
+  );
+}
